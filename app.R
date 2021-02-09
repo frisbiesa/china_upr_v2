23da9a6de57ef9a9085411ff5d2a9dc236d53c18
andresfortunato <63262483+andresfortunato@users.noreply.github.com>
2021-02-09 08:48:41 -0600

    shiny maps start

---
+++ app.R
@@ -0,0 +1,78 @@
+library(tidyverse)
+library(shiny)
+library(maps)
+library(sf)
+library(spData)
+
+debt_stock <- read.csv("debt_stock_china.csv") %>% 
+  select(-country_code, -ISO)
+
+worlde <- world %>% 
+  rename(country = name_long)
+
+mapamundi <- left_join(worlde, debt_stock, by = "country")
+
+
+### shiny app
+
+ui <- fluidPage(
+  fluidRow(
+    column(width = 12,
+           align = "center",
+           tags$h1("Evolution of sovereign debt to China", 
+                   tags$style('head { face: bold }'))
+    )
+  ), 
+  fluidRow(
+    column(width = 12,
+           align = "center",
+           sliderInput(inputId = "date", 
+                           label = "Date:", 
+                           min = 2000, 
+                           max = 2017, 
+                           value = 2000)
+             ),
+    column(width = 12,
+           align = "center",
+           plotOutput("map", 
+                      width = "100%")
+             )
+           )
+  )
+
+
+server <- function(input, output) {
+  
+  data <- reactive({
+    mapamundi %>% 
+      filter(year == input$date) 
+  })
+  
+  output$map <- renderPlot({
+    ggplot() +
+      geom_sf(data = data(), aes(fill = china_debt_gdp)) +
+      labs(title = "", 
+           fill = "",
+           caption = "Source: sonnet") +
+      theme_minimal() +
+      scale_fill_viridis_c() +
+      theme(axis.text.x = element_blank(), 
+            axis.text.y = element_blank(),
+            plot.caption = element_text(size = 12)) +
+      guides(color = FALSE, 
+             alpha = FALSE,
+             fill = guide_colorbar(barwidth = 0.5, 
+                                   barheight = 10, title.position = "left",
+                                   title.theme = element_text(angle = 90),
+                                   title.hjust = .5))
+   
+  },height = 600, width = 600)
+  
+}
+
+
+
+shinyApp(ui = ui, server = server)
+
+
+
