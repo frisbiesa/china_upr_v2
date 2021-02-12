5a247605f75dc382d1ca348736812c886cb7bced
frisbiesa <63260631+frisbiesa@users.noreply.github.com>
2021-02-12 11:20:54 -0600

    new version of shiny map
    includes plotly which is functional with hovertips, but only for y variable

--- app.R
+++ app.R
@@ -1,9 +1,13 @@
 library(tidyverse)
 library(shiny)
 library(maps)
-library(sf)
 library(spData)
 library(lubridate)
+library(devtools)
+library(rnaturalearth)
+library(sp)
+library(plotly)
+library(shinyWidgets)
 
 debt_stock <- read.csv("debt_stock_china.csv") %>% 
   select(-country_code, -ISO)
@@ -12,10 +16,9 @@ debt_stock <- read.csv("debt_stock_china.csv") %>%
 worlde <- world %>% 
   rename(country = name_long)
   
-
 mapamundi <- left_join(worlde, debt_stock, by = "country")
-
-
+mapamundi$debt_usd <- str_remove_all(mapamundi$debt_usd, ",")
+mapamundi$debt_usd <- as.numeric(mapamundi$debt_usd)
 
 ui <- fluidPage(
   fluidRow(
@@ -26,55 +29,75 @@ ui <- fluidPage(
     )
   ), 
   fluidRow(
-    column(width = 12,
+    column(width = 6,
            align = "center",
            sliderInput(inputId = "date", 
                            label = "Date:", 
                            min = 2000 , 
                            max = 2017, 
                            value = 2000)
-             ),
-    column(width = 12,
+             )
+    ,
+    column(width = 6,
+           selectInput(inputId = "fill",
+                       label = "select variable",
+                       c("aggregate debt" = "debt_usd",
+                         "debt to GDP" = "china_debt_gdp"))
+           ),
+    
+    fluidRow(
+      column(width = 12,
            align = "center",
-           plotOutput("map", 
+           plotlyOutput("map", 
                       width = "100%")
              )
            )
   )
+)
 
 
 server <- function(input, output) {
-  
+
   data <- reactive({
     mapamundi %>% 
-      filter(year == input$date) 
+      filter(year == input$date) %>%
+      rename(graph_variable = input$fill)
   })
   
-  output$map <- renderPlot({
-    ggplot() +
-      geom_sf(data = world) +
-      geom_sf(data = data(), aes(fill = china_debt_gdp), alpha = 3/5) +
-      labs(title = "", 
-           fill = "",
-           caption = "Source: sonnet") +
-      theme_minimal() +
-      theme(axis.text.x = element_blank(), 
-            axis.text.y = element_blank(),
-            plot.caption = element_text(size = 12)) +
-      guides(color = FALSE, 
-             alpha = FALSE,
-             fill = guide_colorbar(barwidth = 0.5, 
-                                   barheight = 10, title.position = "left",
-                                   title.theme = element_text(angle = 90),
-                                   title.hjust = .5))
-   
-  },height = 600, width = 600)
+  scale <- reactive({
+    if (input$fill == "debt_usd"){
+      s <- c(1, 200000000)
+    }
+    else if (input$fill == "china_debt_gdp"){
+      s <- c(1, 30)
+    }
+  })
+  
+  plot <- reactive({
+  plt <- ggplot() +
+    geom_sf(data = world) +
+    geom_sf(data = data(), 
+            aes(group = year, fill = graph_variable), 
+            alpha = 3/5) +
+    labs(title = "", 
+         fill = "",
+         caption = 'Source: "China’s Overseas Lending," NBER, Sebastian Horn, Carmen M. Reinhart & Christoph Trebesch') + 
+    scale_fill_viridis_c(option = "magma", limits = scale()) +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(), 
+          axis.text.y = element_blank(),
+          plot.caption = element_text(size = 12))+
+    guides(color = FALSE, alpha = FALSE,
+           fill = guide_colorbar(barwidth = 0.5, 
+                                 barheight = 10, title.position = "left",
+                                 title.theme = element_text(angle = 90),
+                                 title.hjust = .5))
+  plt <- ggplotly(plt, tooltip= c("country", "fill"))
+  })
+  
+  output$map <- renderPlotly({plot()})
   
 }
 
-
-
 shinyApp(ui = ui, server = server)
-
-
 
