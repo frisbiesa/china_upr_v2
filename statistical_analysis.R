4474b75f02906cdc9c932cf88ee63faa3db91ff1
frisbiesa <63260631+frisbiesa@users.noreply.github.com>
2021-03-10 15:58:00 -0600

    intermediate commit
    nearing end of model fitting, but will need to clean up code

--- statistical_analysis.R
+++ statistical_analysis.R
@@ -1,33 +1,31 @@
 library(tidyverse)
 library(stargazer)
-install.packages("stargazer")
+library(mFilter)
+library(plm)
+library(lmtest)
+library(fuzzyjoin)
+
 
 setwd ("~/Desktop/Desktop - MacBook Pro/Data and Programming II/final project/Fortunato-Frisbie-final-project")
 
 joint_support_scores <- read_csv("joint_support_scores.csv")
-
 debt_database <- read_csv("HRT_ChinaDebtStockDatabase.csv", col_names = TRUE)
-
 debt_database <- debt_database %>% select(1:7)
 
-
-target_year <- c("2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017")
-  
-##create a variable of being either over or under the median for Chinese investment 
-
-##there are 16 countries which had 0 Chinese debt to GDP for 2010-2018.  This will be our control group.  
-
+##overall trend of debt
 debt_database %>%
   ggplot(aes(x=Year, y = ChinaDebt_GDP, na.rm = TRUE)) +
   geom_smooth()
 
+##boxplot by year
 debt_database %>%
   ggplot(aes(x=Year, y = ChinaDebt_GDP, na.rm = TRUE)) +
   geom_boxplot(aes(group = Year))
 
 ##make this into a table with stargazer
 
-test <- debt_database %>%
+##this shows there is a lot of positive skewness
+quantiles <- debt_database %>%
   group_by(Year) %>%
   summarise(year_mean = mean(ChinaDebt_GDP, na.rm = TRUE),
             year_median = median(ChinaDebt_GDP, na.rm = TRUE),
@@ -36,26 +34,12 @@ test <- debt_database %>%
  
 ##https://stats.stackexchange.com/questions/76058/specifying-a-difference-in-differences-model-with-multiple-time-periods
 
-debt_database <- debt_database %>% left_join(test, by = "Year")
+debt_database <- debt_database %>% left_join(quantiles, by = "Year")
 
-##the debt levels are skewed right, so for treatment I created a dummy variable based on whether a country 
-##in a given year had over or below the median investment measured as a percentage of GDP.  
-
-debt_database <- debt_database %>%
-  mutate(above_median = if_else(ChinaDebt_GDP > year_median, 1, 0)) %>%
-  mutate(above_mean = if_else(ChinaDebt_GDP > year_mean, 1, 0)) %>%
-  mutate(above_75 = if_else(ChinaDebt_GDP > quantile_75, 1, 0))
-
-debt_database %>%
-  ggplot(aes(x=Year, y = ChinaDebt_GDP, color = above_median)) +
-  geom_smooth(aes(group=above_median), method = "loess")
-
-v <- debt_database %>%
-  group_by(Country) %>%
-  summarise(above_mean = sum(above_mean)) %>%
-  arrange(desc(above_mean))
-
+##the debt levels are skewed right, so here will log china debttoGDP (use log+1 to address zeros)
 ##run ols on level of debt to China in year before voting and overall support score
+
+## use a loop on all of these
 
 debt_2012 <- debt_database %>% filter(Year == 2012)
 debt_2017 <- debt_database %>% filter(Year == 2017)
@@ -66,13 +50,166 @@ score_2018 <- joint_support_scores %>% select(country, support_score_18)
 ols_prep_13 <- debt_2012 %>% left_join(score_2013, by = c("Country" = "country"))
 ols_prep_18 <- debt_2017 %>% left_join(score_2018, by = c("Country" = "country"))
 
-ols_13 <- lm(support_score_13 ~ ChinaDebt_GDP, data = ols_prep_13)
+ols_13 <- lm(support_score_13 ~ log(ChinaDebt_GDP + 1) + GDP_USDbn, data = ols_prep_13)
 stargazer(ols_13, type='text', header=FALSE)
 
 ols_18 <- lm(support_score_18 ~ ChinaDebt_GDP, data = ols_prep_18)
 stargazer(ols_18, type='text', header=FALSE)
 
-##----------------
+##--------------------------------- diff and diff ---------------------------------------
+
+## write a function to make this long for both recs and support
+
+long <- joint_support_scores %>% pivot_longer(Supported_13:total_recs_18, 
+                                      names_to = c("variable", "year"),
+                                      names_pattern = "(.+)_(.+$)",
+                                      values_to = "values")
+
+long$year <- str_replace_all(long$year, "13", "2012")
+long$year <-  str_replace_all(long$year, "18", "2017")
+
+long$year <- as.numeric(long$year)
+long$total_recommendations <- NULL
+
+support_long <-long %>% filter(variable == "support_score")
+
+debt_subset <- debt_database %>% filter(Year == 2012|Year == 2017)
+
+diff_prep <- debt_subset %>% left_join(support_long, by = c("Country" = "country", "Year" = "year"))
+
+diff_prep <- diff_prep %>%
+  mutate(time_dummy = if_else(Year == "2017", 1, 0))
+
+log(diff_prep$ChinaDebt_GDP+1)
+##-------------------------------------cleaning and joining control variables-----------------------
+
+##prep distance to china as a control variable
+
+distance_china <- read_csv("distance_china.csv")
+distance_china$X1 <- NULL
+
+debt_subset <- debt_subset %>% left_join(distance_china, by = c("Country" = "country"), ignore_case = TRUE)
+
+debt_subset$distance[debt_subset$Country == "Cote d'Ivoire"] <- 11255
+
+##
+
+devecon <- read_csv("/Users/sonnetfrisbie/Desktop/Desktop - MacBook Pro/ISDC/R working directory ISDC/DEVECON_fulldataset.csv")
+
+#EIU_TDBT: Total_foreign_debt_[Y]
+#EIU_TDPY: Total_debt/GDP_[Y]
+#WB_ny_gdp_pcap_cd: GDP per capita (current US$)
+
+controls <- devecon %>% 
+  select(iso, year, EIU_TDBT, EIU_TDPY, WB_ny_gdp_pcap_cd) %>% 
+  filter(year == 2012|year == 2017) %>%
+  rename("total_foreign_debt" = "EIU_TDBT", 
+         "total_debt_GDP" = "EIU_TDPY",
+         "GDP_percapita" = "WB_ny_gdp_pcap_cd")
+
+debt_subset <- debt_subset %>% left_join(controls, by = c("ISO" = "iso", "Year" = "year"))
+
+
+##---------------------------------------------------------------------------------------
+
+test <- diff_prep %>% select(Country, Year, ChinaDebt_GDP, GDP_USDbn, values, time_dummy)
+
+test <- pdata.frame(test, index = c("Country", "Year"), drop.index = FALSE)
+
+
+##without FE
+
+ols <- lm(values ~ ChinaDebt_GDP + factor(Country) - 1, data = test)
+
+diff_and_diff_fe <- lm(values ~ time_dummy*log(ChinaDebt_GDP+1) + factor(Country) - 1, data = test)
+
+diff_and_diff <- lm(values ~ time_dummy*log(ChinaDebt_GDP+1), data = test)
+
+test$log_debt_gdp <- log(test$ChinaDebt_GDP+1)
+
+diff_prep
+
+diff_prep %>%
+  drop_na(values) %>%
+  ggplot(aes(x = log(ChinaDebt_GDP+1), y = values)) +
+  geom_point(aes(color = factor(Year))) +
+  geom_smooth(method = "lm", aes(group = factor(Year), color = factor(Year)), se = FALSE, show.legend = TRUE)
+
+##this is significant, but not if we log debt
+coeftest(ols, vcov = vcovHC(ols, type="HC1"))
+
+##these are not, but only measuring the difference between 2013 and 2018
+coeftest(diff_and_diff, vcov = vcovHC(diff_and_diff, type="HC1"))
+coeftest(diff_and_diff_fe, vcov = vcovHC(diff_and_diff_fe, type="HC1"))
+
+##---------------------
+##regressing total recommendations on the log of debt.  It appears that there is a strong negative relationsip
+##between high indebtedness to China and making comments in the UPR
+
+recs_long <- long %>% filter(variable == "total_recs")
+
+recs_long <- debt_subset %>% left_join(recs_long, by = c("Country" = "country", "Year" = "year"))
+
+recs_long <- recs_long %>%
+  mutate(time_dummy = if_else(Year == "2017", 1, 0))
+
+recs_long %>%
+  drop_na(values) %>%
+  ggplot(aes(x = log(ChinaDebt_GDP+1), y = values)) +
+  geom_point(aes(color = factor(Year))) +
+  geom_smooth(method = "lm", aes(group = factor(Year), color = factor(Year)), se = FALSE, show.legend = TRUE)
+
+ols <- lm(values ~ log(ChinaDebt_GDP+1) + distance + GDP_per_capita, data = recs_long)
+
+##note this is robust to controlling for distance from China (which should account for much of the variation in trade) 
+##and also to controlling for total GDP per capita.
+
+stargazer(ols, type='text', header=FALSE)
+
+coeftest(ols, vcov = vcovHC(ols, type="HC1"))
+
+diff_and_diff_fe <- lm(values ~ time_dummy*log(ChinaDebt_GDP+1) + factor(Country) - 1, data = recs_long)
+
+diff_and_diff <- lm(values ~ time_dummy*log(ChinaDebt_GDP+1) + distance + GDP_per_capita + total_debt_GDP, data = recs_long)
+
+coeftest(diff_and_diff, vcov = vcovHC(diff_and_diff, type="HC1"))
+coeftest(diff_and_diff_fe, vcov = vcovHC(diff_and_diff_fe, type="HC1"))
+##run the diff and diff on the total number of comments made
+
+
+
+
+##y = a + treat + time + (treat*time) + controls + e
+
+#time <- [0, 1] for 2013 or 2018
+#treat <- continuous change in Chinese debt between 2013 and 2018
+#treat*time <- diff in diff estimator
+
+##-------------------------------------------HP_test ----------------------------------------------------
+
+
+hp <- debt_database %>% select(Year, year_mean) %>% head(18)
+
+debt_hp <- hpfilter(hp$year_mean, type = c("lambda"), freq = 1)
+
+names(debt_hp)
+
+objectList <- list(debt_hp$x,debt_hp$trend,debt_hp$cycle)
+names(objectList) = c("unemp","trend","cycle")
+sapply(objectList,class)
+
+
+debt_hp %>%
+  ggplot() +
+  geom_line(hp$year_mean)
+
+##coalesce is great for filling in values from another column
+
+##--------------------------
+
+joint_support_scores$country[!(joint_support_scores$country %in% debt_database$Country)]
+
+unique(debt_database$Country)
 
 
 
--- text_analysis.R
+++ text_analysis.R
@@ -2,6 +2,19 @@
 library(tidyverse)
 library(tidytext)
 library(docxtractr)
+library(textdata)
+library(gridExtra)
+library(tm)
+library(SnowballC)
+library(wordcloud)
+library(RColorBrewer)
+library(rvest)
+library(purrr)
+library(readr)
+library(reshape2)
+library(rlist)
+library(varhandle)
+
 
 setwd ("~/Desktop/Desktop - MacBook Pro/Data and Programming II/final project/Fortunato-Frisbie-final-project")
 #------------------------------------------------------------------------------------------
@@ -17,6 +30,10 @@ for (i in filenames) {
   word_tokens_df <- unnest_tokens(text_df, word_tokens,  text, token = "words")
   df <- anti_join(word_tokens_df, stop_words, by = c("word_tokens" = "word"))
   
+  df$word_tokens <- removeNumbers(df$word_tokens)
+  df$word_tokens <-  removePunctuation(df$word_tokens)
+  df <- df %>% filter(word_tokens != "")
+  
   for (s in c("nrc", "afinn", "bing")) {
     df <- df %>% 
       left_join(get_sentiments(s), by = c("word_tokens" = "word")) %>%
@@ -27,7 +44,9 @@ for (i in filenames) {
 
 names(sentiment_list) <- c("Comments About China in 2013", "Comments About China in 2018") # Define names of df's
 
+
 ##using sentiment list in the function in order to allow for automatic title changing 
+
 plot <- function(df){
     ggplot(data = sentiment_list[[df]] %>% filter(!is.na(bing))) +
     geom_histogram(aes(bing), stat = "count") +
@@ -37,14 +56,187 @@ plot <- function(df){
 
 lapply(names(sentiment_list), plot)
 
-#--------------------------------------------------------------------------------------------
+
+
+
+##-----------------------------frequency, lemmas, and wordcloud-----------------------------
+
+freq_list <- list()
+
+for (i in names(sentiment_list)) {
+freq_list[[i]] <- count(sentiment_list[[i]], word_tokens, sort = TRUE)
+}
+
+freq_list[[2]]
+
+##simple bag-of-words approach with lemmas for both documents combined
+
+lemma_list <- list()
+
+for (i in filenames){
+s <- read_file(i)
+text_df <- tibble(text = s)
+df_udp <- udpipe(text_df$text, "english")
+
+test_lemmas <- df_udp %>% 
+  filter(upos != "PUNCT") %>% 
+  anti_join(stop_words, by = c("lemma" = "word"))
+
+test_lemmas$lemma <- removeNumbers(test_lemmas$lemma)
+test_lemmas$lemma <- removePunctuation(test_lemmas$lemma) 
+test_lemma <- test_lemmas %>% select(token, lemma)
+
+lemma_list <- append(lemma_list, list(test_lemma))
+
+}
+
+lemma_list <- list.stack(lemma_list)
+
+
+lemma_count <- print(lemma_list %>%
+                       group_by(lemma) %>%
+                       filter(lemma != "" & lemma != "Ahrcadd" & lemma != "Sdg") %>%
+                       filter(!grepl("(^| ).( |$)", lemma)) %>%
+                       summarise(n = n()) %>%
+                       arrange(desc(n)))
+
+wordcloud(words = lemma_count$lemma, freq = lemma_count$n, min.freq = 10,
+          max.words=500, random.order=FALSE, rot.per=0.35, scale=c(2,0.25),
+          colors=brewer.pal(8, "Dark2"))
+
+dev.copy(png,'wordcloud.png')
+dev.off()
+
+#-----------------------------text cleaning trail before generalization----------------------------
 
 recommendations_2013 <- read_docx("Matrix_recommendations_2013.docx")
 recommendations_2013 <- docx_extract_all_tbls(recommendations_2013)
 
-recommendations_2018 <- read_docx("Matrix_recommendations_2018.docx")
-recommendations_2018 <- docx_extract_all_tbls(recommendations_2018)
+recommendations_2013 <- as_tibble(recommendations_2013[[1]])
+recommendations_2013$Assessment.comments.on.level.of.implementation <- NULL
+
+##removing all rows which start with "theme"
+recommendations_2013 <-recommendations_2013 %>% 
+  filter(!str_detect(Recommendation, 'Theme:'))
+
+##splitting off first codes
+y <-  colsplit(recommendations_2013$Recommendation," ",c("code","recommendations"))
+
+##binding back together
+recommendations_2013 <- cbind(recommendations_2013, y)
+
+##dropping original column
+recommendations_2013$Recommendation <- NULL
+
+##use regex to remove ending redundant information from recommendations
+##Source: https://r4ds.had.co.nz/strings.html
+recommendations_2013$recommendations <- str_extract(recommendations_2013$recommendations, "(.*);")
+
+##found one instance where a typo in punctuation removed the string
+sum(is.na(recommendations_2013$recommendations))
+
+##adding back in
+line_49 <- "Develop programme for sharing of its experiences in addressing the right to development with African countries in the context of the Forum on China-Africa cooperation (Sierra Leone)"
+
+recommendations_2013[49,4] <- line_49
+
+##testing separating country comments
+recommendations_2013 <- separate(recommendations_2013, col = recommendations, into = c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"), sep = ";", remove = FALSE)
+
+##replacing empty strings with NA
+recommendations_2013[recommendations_2013==""]<-NA
+recommendations_2013$recommendations <- NULL
+
+recommendations_2013 <- recommendations_2013 %>% pivot_longer(cols = a:k, values_to = "recommendations", values_drop_na = TRUE)
+
+recommendations_2013$name <- NULL
+
+##removing ()
+recommendations_2013$country <- str_extract(recommendations_2013$recommendations, "(?<=\\().+?(?=\\))")
+recommendations_2013$recommendations <- str_remove(recommendations_2013$recommendations, "\\([^()]*\\)")
+
+##source: https://stackoverflow.com/questions/640001/how-can-i-remove-text-within-parentheses-with-a-regex
+
+###for write-up, text analysis in french https://content.sciendo.com/configurable/contentpage/journals$002fadhi$002f3$002f1$002farticle-p112.xml
+
+recommendations_2013 <- separate(recommendations_2013, col = Full.list.of.themes, into = c("a", "b", "c", "d", "e", "f", "g", "h"), sep = "(?<=.)(?=\\D{1}\\d{2})", remove = TRUE)
+
+##sum(!is.na(test$h)) seeing if got enough columns
+
+##replacing empty strings with NA
+recommendations_2013[recommendations_2013==""]<-NA
+recommendations_2013$name <- NULL
+
+recommendations_2013 <- recommendations_2013 %>% pivot_longer(cols = a:h, values_to = "themes", values_drop_na = TRUE)
+
+##source: https://www.r-bloggers.com/2018/04/strsplit-but-keeping-the-delimiter/
+
+### ------------------------ generalizing data cleaning ---------------------------------
 
 
+filenames_docs <- c("Matrix_recommendations_2013.docx", "Matrix_recommendations_2018.docx")
+text_cleaning_list <- list()
+
+for (i in filenames_docs){
+  s <- read_docx(i)
+  s <- docx_extract_all_tbls(s)
+  s <- as_tibble(s[[1]])
+  s$Assessment.comments.on.level.of.implementation <- NULL
+  
+  ##removing all rows which start with "theme"
+  
+  s <- s %>% 
+    filter(!str_detect(Recommendation, 'Theme:'))
+  
+  ##splitting off first codes
+  y <-  colsplit(s$Recommendation," ",c("code","recommendations"))
+  
+  ##binding back together
+  s <- cbind(s, y)
+  
+  ##dropping original column
+  s$Recommendation <- NULL
+  
+  ##use regex to remove ending redundant information from recommendations
+  ##Source: https://r4ds.had.co.nz/strings.html
+  s$recommendations <- str_extract(s$recommendations, "(.*);")
+  
+  ##separating country comments
+  s <- separate(s, col = recommendations, into = c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"), sep = ";", remove = FALSE)
+  
+  ##replacing empty strings with NA
+  s[s==""]<-NA
+  s$recommendations <- NULL
+  
+  s <- s %>% pivot_longer(cols = a:k, values_to = "recommendations", values_drop_na = TRUE)
+  
+  s$name <- NULL
+  
+  ##removing ()
+  s$country <- str_extract(s$recommendations, "(?<=\\().+?(?=\\))")
+  s$recommendations <- str_remove(s$recommendations, "\\([^()]*\\)")
+  
+  ##cleaning theme column and separating
+  s <- separate(s, col = Full.list.of.themes, into = c("a", "b", "c", "d", "e", "f", "g", "h"), sep = "(?<=.)(?=[a-zA-Z]\\d{1,2})", remove = TRUE)
+  s <- s %>% pivot_longer(cols = a:h, values_to = "themes", values_drop_na = TRUE)
+  
+  s[s==""]<-NA
+  s$name <- NULL
+  
+  ##saving a list of themes
+  theme_list <- unique(s$themes)
+  theme_list <- as_tibble(theme_list)
+  
+  s <- separate(s, col = themes, into = c("theme_code", "description"), sep = "\\s|\\-", extra = "merge", remove = TRUE)
+  
+  names(s) <- tolower(names(s))
+  
+  text_cleaning_list <- append(text_cleaning_list, list(s))
+}
+
+names(text_cleaning_list) <- filenames_docs  # Define names of df's
+
+write_csv(text_cleaning_list[[1]], "text_cleaning_list_2013.csv")
+write_csv(text_cleaning_list[[2]], "text_cleaning_list_2018.csv")
 
 
